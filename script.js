d164f704042a9604b19ca0ca20eaf41ea1c199a4
Glitch (glitch-hello-website) <none>
2023-11-07 04:02:01 +0000

    🍐🎢 Checkpoint ./script.js:62725575/1862:62725838/1 ./index.html:62725838/6 ./princesas.html:62725838/1128 ./paises.html:62725575/955

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function mostrarMensaje() {
+/*function mostrarMensaje() {
   const nombre = document.getElementById("inputNombre").value;
   const seleccionTrivia = document.getElementById("seleccionTrivia");
   const opcionSeleccionada = seleccionTrivia.value;
@@ -10,7 +10,7 @@ function mostrarMensaje() {
     //const triviaSeleccionada = opcionSeleccionada.value;
   
     const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${opcionSeleccionada}. Mucha suerte!`;
-    document.getElementById("mensaje").textContent = mensaje;
+    //document.getElementById("mensaje").textContent = mensaje;
     sessionStorage.setItem("mensaje", mensaje); // Almacenar el mensaje en sessionStorage
     window.location.href = "paises.html";
   } else {
@@ -25,9 +25,46 @@ const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
 
 botonMostrarMensaje.addEventListener("click", mostrarMensaje);
 
+*/
 
 
-/*document.addEventListener("DOMContentLoaded", function() {
+//otro
+function mostrarMensaje() {
+  const nombre = document.getElementById("inputNombre").value;
+  const seleccionTrivia = document.getElementById("seleccionTrivia");
+  const opcionSeleccionada = seleccionTrivia.value;
+  console.log(opcionSeleccionada);
+  
+
+  if (nombre && opcionSeleccionada) {
+    let redireccion = ""; // Variable para almacenar la URL de redirección
+    
+    if (opcionSeleccionada === "paises") {
+      redireccion = "paises.html";
+    } else if (opcionSeleccionada === "princesas") {
+      redireccion = "princesas.html";
+    }
+    
+    if (redireccion) {
+      const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${opcionSeleccionada}. Mucha suerte!`;
+      sessionStorage.setItem("mensaje", mensaje); // Almacenar el mensaje en sessionStorage
+      window.location.href = redireccion; // Redirigir a la página correspondiente
+    } else {
+      alert("Opción no válida. Debes seleccionar una trivia válida.");
+    }
+  } else {
+    alert("Debes ingresar ambos datos para continuar.");
+  }
+}
+
+//boton
+const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
+
+botonMostrarMensaje.addEventListener("click", mostrarMensaje);
+
+
+//Session Storage
+document.addEventListener("DOMContentLoaded", function() {
   const mensajeElement = document.getElementById("mensaje");
   const mensaje = sessionStorage.getItem("mensaje");
   if (mensaje) {
@@ -35,4 +72,4 @@ botonMostrarMensaje.addEventListener("click", mostrarMensaje);
   } else {
     mensajeElement.textContent = "Mensaje no encontrado.";
   }
-});*/
+});
