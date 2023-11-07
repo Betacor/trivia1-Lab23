a8790294309676ef59547a1ffe59abb75bd5369d
Glitch (glitch-hello-website) <none>
2023-11-07 04:14:04 +0000

    🍛🙈 Checkpoint ./script.js:62725575/960:62725838/204 ./princesas.html:62725575/24 ./paises.html:62725575/1135 ./index.html:62725575/1023

--- script.js
+++ script.js
@@ -26,43 +26,6 @@ const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
 botonMostrarMensaje.addEventListener("click", mostrarMensaje);
 
 */
-
-
-//otro
-function mostrarMensaje() {
-  const nombre = document.getElementById("inputNombre").value;
-  const seleccionTrivia = document.getElementById("seleccionTrivia");
-  const opcionSeleccionada = seleccionTrivia.value;
-  console.log(opcionSeleccionada);
-  
-
-  if (nombre && opcionSeleccionada) {
-    let redireccion = ""; // Variable para almacenar la URL de redirección
-    
-    if (opcionSeleccionada === "paises") {
-      redireccion = "paises.html";
-    } else if (opcionSeleccionada === "princesas") {
-      redireccion = "princesas.html";
-    }
-    
-    if (redireccion) {
-      const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${opcionSeleccionada}. Mucha suerte!`;
-      sessionStorage.setItem("mensaje", mensaje); // Almacenar el mensaje en sessionStorage
-      window.location.href = redireccion; // Redirigir a la página correspondiente
-    } else {
-      alert("Opción no válida. Debes seleccionar una trivia válida.");
-    }
-  } else {
-    alert("Debes ingresar ambos datos para continuar.");
-  }
-}
-
-//boton
-const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
-
-botonMostrarMensaje.addEventListener("click", mostrarMensaje);
-
-
 //Session Storage
 document.addEventListener("DOMContentLoaded", function() {
   const mensajeElement = document.getElementById("mensaje");
@@ -73,3 +36,46 @@ document.addEventListener("DOMContentLoaded", function() {
     mensajeElement.textContent = "Mensaje no encontrado.";
   }
 });
+
+
+
+//otro
+function mostrarMensaje() {
+  const nombre = document.getElementById("inputNombre").value;
+  const seleccionTrivia = document.getElementById("seleccionTrivia");
+  const opcionSeleccionada = seleccionTrivia.value;
+  console.log(opcionSeleccionada);
+  //const mensajeElement = document.getElementById("mensaje");
+  
+
+  if (nombre && opcionSeleccionada) {
+  //const triviaSeleccionada = seleccionTrivia.options[seleccionTrivia.selectedIndex].text;
+    //const triviaSeleccionada = opcionSeleccionada.value;
+    let redireccion = "";
+    
+    if (opcionSeleccionada === "paises") {
+      redireccion = "paises.html";
+    } else if (opcionSeleccionada === "princesas") {
+      redireccion = "princesas.html";
+    }
+    
+    if (redireccion) {
+      const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${opcionSeleccionada}. Mucha suerte!`;
+      //document.getElementById("mensaje").textContent = mensaje;
+      sessionStorage.setItem("mensaje", mensaje); //guardar mensaje en sessionStorage
+      window.location.href = redireccion; // redirigir a la pagina trivia
+    } else {
+      alert("Opción no válida. Debes seleccionar una trivia válida.");
+    }
+  } else {
+    // Mensaje alerta
+    alert("Debes ingresar ambos datos para continuar.");
+  }
+}
+
+//boton
+const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
+
+botonMostrarMensaje.addEventListener("click", mostrarMensaje);
+
+
