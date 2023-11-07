e861c8914b841c35d694b6f9da693a8d6cd7b4b9
Glitch (glitch-hello-website) <none>
2023-11-07 02:17:03 +0000

    🎾🏨 Checkpoint ./script.js:62725575/13:62725838/1599

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-//función Saludo + Seleccion de Trivia
-
-/*function mostrarMensaje() {
-  const nombre = document.getElementById("inputNombre").value;
-  const trivia = document.getElementById("triviaSeleccionada").textContent;
-  
-  
-  const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${trivia}. Mucha suerte!`;
-  document.getElementById("mensaje").textContent = mensaje;
-}
-
-
-function seleccionarTrivia(trivia) {
-  document.getElementById("triviaSeleccionada").textContent = trivia;
-  mostrarMensaje();
-}*/
-
-//aqui empieza otra opcion
-
-/*
-// Obtén una referencia al botón por su ID
-const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
-
-// Agrega un manejador de eventos al botón para llamar a la función mostrarMensaje
-botonMostrarMensaje.addEventListener("click", mostrarMensaje);
-
 
 
 function mostrarMensaje() {
   const nombre = document.getElementById("inputNombre").value;
   const seleccionTrivia = document.getElementById("seleccionTrivia");
   const opcionSeleccionada = seleccionTrivia.value;
-  const mensajeElement = document.getElementById("mensaje");
+  //const mensajeElement = document.getElementById("mensaje");
   
-  if (opcionSeleccionada) {
-  const triviaSeleccionada = seleccionTrivia.options[seleccionTrivia.selectedIndex].text;
+  if (nombre && opcionSeleccionada) {
+  //const triviaSeleccionada = seleccionTrivia.options[seleccionTrivia.selectedIndex].text;
     //const triviaSeleccionada = opcionSeleccionada.value;
   
     const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${opcionSeleccionada}. Mucha suerte!`;
@@ -43,11 +17,20 @@ function mostrarMensaje() {
     alert("Debes seleccionar una trivia para continuar.");
   }
 }
+
+
+// Obtén una referencia al botón por su ID
+const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
+
 document.getElementById("seleccionTrivia").addEventListener("change", mostrarMensaje);
-*/
 
 
-//otra opcion
+
+
+
+
+
+/*//otra opcion
 function mostrarMensaje() {
   const nombre = document.getElementById("inputNombre").value;
   const seleccionTrivia = document.getElementById("seleccionTrivia");
@@ -59,7 +42,7 @@ function mostrarMensaje() {
     document.getElementById("mensaje").textContent = mensaje;
   } else {
     // Manejar el caso en el que no se haya ingresado un nombre o seleccionado una opción
-    document.getElementById("mensaje").textContent = ""; // Limpiar el mensaje
+    //document.getElementById("mensaje").textContent = ""; // Limpiar el mensaje
     alert("Debes ingresar un nombre y seleccionar una trivia antes de presionar 'Empezar a Jugar'.");
   }
 }
@@ -68,4 +51,4 @@ function mostrarMensaje() {
 const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
 
 // Agrega un manejador de eventos al botón para llamar a la función mostrarMensaje cuando se haga clic
-botonMostrarMensaje.addEventListener("click", mostrarMensaje);
+botonMostrarMensaje.addEventListener("click", mostrarMensaje);*/
