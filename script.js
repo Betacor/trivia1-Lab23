7b78c0aa37f87ce02886b7c8ac079b1bf92ae4df
Glitch (glitch-hello-website) <none>
2023-11-07 00:17:00 +0000

    🗾🐔 Checkpoint ./index.html:62725575/502 ./script.js:62725575/514:62725838/384

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const inputNombre = document.getElementById('inputNombre');
+//const inputNombre = document.getElementById('inputNombre');
 
+function mostrarMensaje() {
+  const nombre = document.getElementById("inputNombre").value;
+  const trivia = document.getElementById("triviaSeleccionada").textContent;
+  const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${trivia}. Mucha suerte!`;
+  document.getElementById("mensaje").textContent = mensaje;
+}
+
+function seleccionarTrivia(trivia) {
+  document.getElementById("triviaSeleccionada").textContent = trivia;
+  mostrarMensaje();
+}
