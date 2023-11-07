aa95a4f92884c7e98d88e0fdabfa6ad11c22f925
Glitch (glitch-hello-website) <none>
2023-11-07 01:53:02 +0000

    🏵🏁 Checkpoint ./script.js:62725575/6599:62725838/45 ./index.html:62725838/5 ./style.css:62725838/10

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function seleccionarTrivia(trivia) {
 
 
 
-
+/*
 // Obtén una referencia al botón por su ID
 const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
 
@@ -33,13 +33,35 @@ function mostrarMensaje() {
   console.log(seleccionTrivia);
   
   if (opcionSeleccionada) {
-    const triviaSeleccionada = seleccionTrivia.value;
+    const triviaSeleccionada = opcionSeleccionada.value;
   
-    const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${triviaSeleccionada}. Mucha suerte!`;
+    const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${opcionSeleccionada}. Mucha suerte!`;
     document.getElementById("mensaje").textContent = mensaje;
   } else {
     // Manejar el caso en el que no se ha seleccionado ninguna opción
     alert("Debes seleccionar una trivia para continuar.");
   }
 }
+*/
 
+// Obtén una referencia al botón por su ID
+const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
+
+// Agrega un manejador de eventos al botón para llamar a la función mostrarMensaje cuando se haga clic
+botonMostrarMensaje.addEventListener("click", mostrarMensaje);
+
+function mostrarMensaje() {
+  const nombre = document.getElementById("inputNombre").value;
+  const seleccionTrivia = document.getElementById("seleccionTrivia");
+  const triviaSeleccionada = seleccionTrivia.value;
+
+  if (nombre && triviaSeleccionada) {
+    // Verifica que se haya ingresado un nombre y se haya seleccionado una opción
+    const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${triviaSeleccionada}. Mucha suerte!`;
+    document.getElementById("mensaje").textContent = mensaje;
+  } else {
+    // Manejar el caso en el que no se haya ingresado un nombre o seleccionado una opción
+    document.getElementById("mensaje").textContent = ""; // Limpiar el mensaje
+    alert("Debes ingresar un nombre y seleccionar una trivia antes de presionar 'Empezar a Jugar'.");
+  }
+}
