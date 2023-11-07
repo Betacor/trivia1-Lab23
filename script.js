db19c24f2a4e92eab9a23e8f4cd8eb807d5243ec
Glitch (glitch-hello-website) <none>
2023-11-07 23:03:48 +0000

    🚀💎 Checkpoint ./paises.html:62725575/4406 ./princesas.html:62725838/189 ./script.js:62725838/1143

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-/*function mostrarMensaje() {
-  const nombre = document.getElementById("inputNombre").value;
-  const seleccionTrivia = document.getElementById("seleccionTrivia");
-  const opcionSeleccionada = seleccionTrivia.value;
-  console.log(opcionSeleccionada);
-  //const mensajeElement = document.getElementById("mensaje");
-  
-  if (nombre && opcionSeleccionada) {
-  //const triviaSeleccionada = seleccionTrivia.options[seleccionTrivia.selectedIndex].text;
-    //const triviaSeleccionada = opcionSeleccionada.value;
-  
-    const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${opcionSeleccionada}. Mucha suerte!`;
-    //document.getElementById("mensaje").textContent = mensaje;
-    sessionStorage.setItem("mensaje", mensaje); // Almacenar el mensaje en sessionStorage
-    window.location.href = "paises.html";
-  } else {
-    // Mensaje alerta
-    alert("Debes ingresar ambos datos para continuar.");
-  }
-}
-
-
-//boton
-const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
-
-botonMostrarMensaje.addEventListener("click", mostrarMensaje);
-*/
-
-
+//INICIO
 //Recupera datos desde el Session Storage
 document.addEventListener("DOMContentLoaded", function() {
   const mensajeElement = document.getElementById("mensaje");
@@ -81,3 +53,5 @@ const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
 botonMostrarMensaje.addEventListener("click", mostrarMensaje);
 
 
+
+//PREGUNTAS
