1864916a18ebf02a40c6e89645ca01c05b5688af
Glitch (glitch-hello-website) <none>
2023-11-07 02:51:58 +0000

    🏗🐜 Checkpoint ./script.js:62725838/103 ./princesas.html:62725838/892 ./paises.html:62725575/830

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-
 function mostrarMensaje() {
   const nombre = document.getElementById("inputNombre").value;
   const seleccionTrivia = document.getElementById("seleccionTrivia");
   const opcionSeleccionada = seleccionTrivia.value;
+  console.log(opcionSeleccionada);
   //const mensajeElement = document.getElementById("mensaje");
   
   if (nombre && opcionSeleccionada) {
@@ -13,13 +12,13 @@ function mostrarMensaje() {
     const mensaje = `Hola ${nombre}, has ingresado a la trivia de ${opcionSeleccionada}. Mucha suerte!`;
     document.getElementById("mensaje").textContent = mensaje;
   } else {
-    // Men
+    // Mensaje alerta
     alert("Debes ingresar ambos datos para continuar.");
   }
 }
 
 
-// Obtén una referencia al botón por su ID
+//boton
 const botonMostrarMensaje = document.getElementById("mostrarMensajeButton");
 
 botonMostrarMensaje.addEventListener("click", mostrarMensaje);
